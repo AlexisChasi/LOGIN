da0cc521e5c6df0661970b2b9f81c6d6c465d057
alexis <alexis.fercho@hotmail.com>
2023-02-03 21:33:01 -0500

    Divisas aun falta

---
+++ src/divisas.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class divisas extends login{
+    public JPanel convertidorDivisas;
+    private JSlider slider1;
+    private JComboBox decb;
+    private JComboBox acb;
+    private JLabel leyenda;
+    private JLabel cantidadlbl;
+
+    public divisas() {
+
+
+
+
+    }
+}
